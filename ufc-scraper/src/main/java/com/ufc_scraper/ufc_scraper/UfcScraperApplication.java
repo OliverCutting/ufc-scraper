093100759e1a336e680c998e7a99284febc1a191
Oliver Patrick Cutting <oliver.cutting@gmail.com>
2025-11-16 14:57:48 +0000

    add base java project
    Signed-off-by: Oliver Patrick Cutting <oliver.cutting@gmail.com>

---
+++ ufc-scraper/src/main/java/com/ufc_scraper/ufc_scraper/UfcScraperApplication.java
@@ -0,0 +1,13 @@
+package com.ufc_scraper.ufc_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfcScraperApplication.class, args);
+	}
+
+}
